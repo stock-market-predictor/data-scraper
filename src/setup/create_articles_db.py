2679ae0439cd7b22666085cffd919e9e2c88bcbc
thisistrivial <g.lee13770334@gmail.com>
2020-07-20 22:07:00 -0400

    Added sec table

--- src/setup/create_articles_db.py
+++ src/setup/create_articles_db.py
@@ -3,7 +3,11 @@ import db_handler
  
 def create_news(cursor):
   cursor.execute("""CREATE TABLE news
-                (news_id INTEGER PRIMARY KEY, title TEXT, body TEXT)""")
+                (news_id INTEGER PRIMARY KEY, date TEXT, source TEXT, title TEXT, body TEXT)""")
 
-db_handler.connect("articles.db", create_news)
+def create_sec(cursor):
+  cursor.execute("""CREATE TABLE sec
+                (sec_id INTEGER PRIMARY KEY, date TEXT, source TEXT, title TEXT, body TEXT)""")
+
+db_handler.connect("articles.db", create_news, create_sec)
 
